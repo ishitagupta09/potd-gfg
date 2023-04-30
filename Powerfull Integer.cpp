a98ca6f147eef570738b9119dd5312c548157c23
Ishita Gupta <124298301+ishitagupta09@users.noreply.github.com>
2023-04-30 21:53:17 +0530

    Create Powerfull Integer.cpp

---
+++ Powerfull Integer.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:
+    int powerfullInteger(int n,vector<vector<int>> &i, int k) {
+        map <int , int> mp ;
+        for(int j = 0 ; j < n ; j++){
+            mp[i[j][0]]++ ;
+            mp[i[j][1]+1]-- ;
+        }
+        int cnt = 0 , ans = -1 ;
+        for(auto &x : mp){
+            if(cnt >= k){
+                ans = max(ans , x.first - 1) ;
+            }
+            cnt += x.second ;
+        }
+        return ans ;
+    }
+};
+
