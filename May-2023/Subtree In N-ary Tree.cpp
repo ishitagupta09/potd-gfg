289de4e34c8839c1a9acbe0f1bc32693dee1edd0
Ishita Gupta <124298301+ishitagupta09@users.noreply.github.com>
2023-05-01 22:11:54 +0530

    Create Subtree In N-ary Tree.cpp

---
+++ May-2023/Subtree In N-ary Tree.cpp
@@ -0,0 +1,43 @@
+class Solution{
+public:
+      string cal(Node* root, map<string, int >&m )
+      {
+          
+          if(root->children.size()==0)
+          {
+              m[to_string(root->data)]++;
+              return to_string(root->data) ;
+          }
+          string ans ="" ;
+          for(auto it: root->children)
+          {
+              string x = cal(it, m );
+              ans+= "+"+x ; 
+          }
+          ans =ans.substr(1) ;
+          ans=to_string(root->data)+"+"+ans ;
+            m[ans]++; 
+          
+        return ans ; 
+          
+      }
+
+
+    int duplicateSubtreeNaryTree(Node *root){
+        
+        // Code here
+        map<string , int >m ; 
+        cal(root , m ) ; 
+        int ans = 0 ;
+        for(auto it: m )
+        {
+            if(it.second>1)
+            {
+               
+                ans++ ;}
+                
+            
+        }
+        return ans ; 
+    }
+};
